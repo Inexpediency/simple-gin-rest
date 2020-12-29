3fa519469f00133f3d7bd18ca21f73402851c06e
Ythosa <vasus714@yandex.ru>
2020-12-29 15:48:16 +0300

    Added basic auth

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	controller "Inexpediency/simple-gin-rest/contoller"
+	"Inexpediency/simple-gin-rest/middleware"
 	"Inexpediency/simple-gin-rest/service"
 	"github.com/gin-gonic/gin"
 	"io"
@@ -23,7 +24,11 @@ func main() {
 	setupLogOutput()
 
 	server := gin.New()
-	server.Use(gin.Recovery(), gin.Logger())
+	server.Use(
+		gin.Recovery(),
+		gin.Logger(),
+		middleware.BasicAuth(),
+	)
 
 	server.POST("/video", videoController.Save)
 	server.GET("/videos", videoController.FindAll)
